8bc551188ef5a9d516fef94823834db577a129c6
edjinbauan <ejbauan@gmail.com>
2023-08-30 19:45:24 +0800

    Price Dynamics

---
+++ js/price-rates-data.js
@@ -0,0 +1,12 @@
+//    WEEKENDER PRICE RATES
+const arr = [
+    "John", 
+    "Peter", 
+    "Sally", 
+    "Jane"
+    ];
+   const weekender1 = 2; // Index of the name you want to display
+   document.getElementById("weekender1").innerHTML = arr[weekender1];
+   const weekender2 = 0; // Index of the name you want to display
+   document.getElementById("weekender2").innerHTML = arr[weekender2];
+//    WEEKENDER PRICE RATES
