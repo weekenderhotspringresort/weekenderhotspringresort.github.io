a13fbf08be04f3b72eb43076b4ba4d9ff1cea076
edjinbauan <ejbauan@gmail.com>
2023-09-01 18:33:31 +0800

    Calendar and JS Price

--- js/price-rates-data.js
+++ js/price-rates-data.js
@@ -1,12 +1,41 @@
 //    WEEKENDER PRICE RATES
-const arr = [
-    "John", 
-    "Peter", 
-    "Sally", 
-    "Jane"
+
+const rates = [
+    //WEEKENDER 1 HOT SPRING RESORT
+    "₱15,000.00", // (0) WEEKENDER 1 MONDAY-THURSDAY PRICE
+    "₱20,000.00", // (1) WEEKENDER 1 FRIDAY-SUNDAY PRICE
+
+    //WEEKENDER 2 HOT SPRING RESORT
+    "₱25,000.00", // (2) WEEKENDER 2 MONDAY-THURSDAY PRICE
+    "₱28,000.00", // (3) WEEKENDER 2 FRIDAY-SUNDAY PRICE
+
+    //WEEKENDER 3 HOT SPRING RESORT
+    "₱20,000.00", // (4) WEEKENDER 3 MONDAY-THURSDAY PRICE
+    "₱25,000.00", // (5) WEEKENDER 3 FRIDAY-SUNDAY PRICE
+
+    //WEEKDAYS HOT SPRING RESORT
+    "₱10,000.00", // (6) WEEKENDER 2 MONDAY-THURSDAY PRICE
+    "₱13,000.00" // (7) WEEKENDER 2 FRIDAY-SUNDAY PRICE
     ];
-   const weekender1 = 2; // Index of the name you want to display
-   document.getElementById("weekender1").innerHTML = arr[weekender1];
-   const weekender2 = 0; // Index of the name you want to display
-   document.getElementById("weekender2").innerHTML = arr[weekender2];
+    
+   const weekender1weekdays = 0; 
+   document.getElementById("weekender1weekdays").innerHTML = rates[weekender1weekdays];
+   const weekender1weekends = 1; 
+   document.getElementById("weekender1weekends").innerHTML = rates[weekender1weekends];
+
+   const weekender2weekdays = 2; 
+   document.getElementById("weekender2weekdays").innerHTML = rates[weekender2weekdays];
+   const weekender2weekends = 3; 
+   document.getElementById("weekender2weekends").innerHTML = rates[weekender2weekends];
+
+   const weekender3weekdays = 4; 
+   document.getElementById("weekender3weekdays").innerHTML = rates[weekender3weekdays];
+   const weekender3weekends = 5; 
+   document.getElementById("weekender3weekends").innerHTML = rates[weekender3weekends];
+
+   const weekdaysresortweekdays = 6; 
+   document.getElementById("weekdaysresortweekdays").innerHTML = rates[weekdaysresortweekdays];
+   const weekdaysresortends = 7; 
+   document.getElementById("weekdaysresortweekends").innerHTML = rates[weekdaysresortends];
+   
 //    WEEKENDER PRICE RATES
